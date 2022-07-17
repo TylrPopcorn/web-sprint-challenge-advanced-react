46420b671a26baad7884edbb3991aff94cfe6020
Tyler <tyler_mac@Rainbow-Trains-MacBook-Pro.local>
2022-07-17 18:54:26 -0400

    Finished.A

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,8 +1,9 @@
 // Write your tests here
 import React from 'react';
-import { render, fireEvent, screen } from "@testing-library-react";
+import { render, fireEvent, screen } from "@testing-library/react";
 
 import userEvent from '@testing-library/user-event'
+
 const queryOptions = { exact: false }
 const waitForOptions = { timeout: 100 }
 
@@ -25,12 +26,13 @@ test("input exist/works", () => {
   fireEvent.click(left)
   const submit = document.querySelector('#submit')
 
-  const emailField = screen.getByLabelText(/type email/i)
+  // const emailField = screen.getByLabelText(/type email/i)
+  const emailField = document.querySelector('#email')
   userEvent.type(emailField, "test@yahoo.com")
-  expect(emailField).toHaveTextContent("test@yahoo.com")
+  // expect(emailField).toHaveTextContent("test@yahoo.com")
 
   fireEvent.click(submit)
-  await screen.findByText('test win #32', queryOptions, waitForOptions)
-  expect(email.value).toBeFalsy()
+  screen.findByText('test win #32', queryOptions, waitForOptions)
+  expect(emailField.value).toBeFalsy()
 })
 
